dc499ff4f2cc7f3d26aa1a6ac4ca6fda0b034673
Tee <ttssahlberg@gmail.com>
2014-09-13 15:00:13 +0300

    eteenpäin

---
+++ INRavustaja/src/main/java/inravustaja/sovelluslogiikka/Tabletit.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package inravustaja.sovelluslogiikka;
+
+/**
+ *
+ * @author onnikone
+ */
+public class Tabletit {
+    
+    private double tabletit;
+    
+    public Tabletit(double maara){
+        this.tabletit = maara;
+    }
+    
+    public double getTabletit(){
+        return this.tabletit;
+    }
+    
+}
