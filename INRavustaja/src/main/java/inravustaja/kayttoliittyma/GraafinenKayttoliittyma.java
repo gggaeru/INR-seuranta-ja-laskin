8dd0a5ecf3f5862887346f4190a060c6e2bc396b
Tee <ttssahlberg@gmail.com>
2014-10-09 22:28:58 +0300

    viikko6

--- INRavustaja/src/main/java/inravustaja/kayttoliittyma/GraafinenKayttoliittyma.java
+++ INRavustaja/src/main/java/inravustaja/kayttoliittyma/GraafinenKayttoliittyma.java
@@ -11,6 +11,9 @@ import javax.swing.JFrame;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -22,6 +25,7 @@ import javax.swing.WindowConstants;
 public class GraafinenKayttoliittyma implements Runnable{
     
      private JFrame frame; 
+    private Object container;
 
     @Override
     public void run() {
@@ -29,7 +33,11 @@ public class GraafinenKayttoliittyma implements Runnable{
          frame.setPreferredSize(new Dimension(300, 150));
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
          
-        luoKomponentit(frame.getContentPane());
+         try {
+             luoKomponentit(frame.getContentPane());
+         } catch (IOException ex) {
+             Logger.getLogger(GraafinenKayttoliittyma.class.getName()).log(Level.SEVERE, null, ex);
+         }
  
         frame.pack();
         frame.setVisible(true);
@@ -38,13 +46,30 @@ public class GraafinenKayttoliittyma implements Runnable{
         
     }
 
-    private void luoKomponentit(Container con) {
+    private void luoKomponentit(Container con) throws IOException {
        frame.setLayout(new GridLayout(3, 1));
         JTextField tuloskentta = new JTextField("Tervetuloa INR-avustajaan! Anna salasana:");
         con.add(tuloskentta);
         tuloskentta.setEnabled(false);
           JTextField syotekentta = new JTextField("");
         con.add(syotekentta);
+        
+         JButton uusiarvo = new JButton("Uusi arvo");
+        JButton tarkasta = new JButton("Tarkasta edellinen");
+        JButton ok = new JButton("Ok");
+        JButton eitallenna = new JButton("Ei tallenna");
+         
+        
+         Tapahtumakuuntelija kasittelija = new Tapahtumakuuntelija(uusiarvo, tarkasta, ok, eitallenna, tuloskentta, syotekentta);
+         
+        uusiarvo.addActionListener(kasittelija);
+        tarkasta.addActionListener(kasittelija);
+        ok.addActionListener(kasittelija);
+       
+        JPanel paneli = new JPanel(new GridLayout(1, 3));
+        paneli.add(uusiarvo);
+        paneli.add(tarkasta);
+        paneli.add(ok);
         
     }
     
